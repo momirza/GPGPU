6ea77c1a611aacb8d9d9f30586657f1808242fdf
Mo Mirza <mo@momirza.co.uk>
2013-03-20 05:48:18 +0000

    added device parmeters file and made function parameters work

--- src/functions.h
+++ src/functions.h
@@ -16,7 +16,7 @@ __device__ float F0(const float *x, const float *params)
 	k=2 (bi-variate)
 	params = [o1,o2]  (2 parameters)
 */
-float F1(const float *x, const float *params)
+__device__ float F1(const float *x, const float *params)
 {
 	return sin(params[0] + x[0]*x[1]) * exp(params[1]+x[0]);
 }
@@ -26,7 +26,7 @@ float F1(const float *x, const float *params)
 	k=3 (tri-variate)
 	params = []  (no parameters)
 */
-float F2(const float *x, const float *params)
+__device__ float F2(const float *x, const float *params)
 {
 	return round(exp(-x[0]))-round(exp(x[1]))*sin(x[2]);
 }
@@ -36,7 +36,7 @@ float F2(const float *x, const float *params)
 	k=3 (tri-variate)
 	params = [norm_lev]  (1 parameter)
 */
-float F3(const float *x, const float *params)
+__device__ float F3(const float *x, const float *params)
 {
 	// Perform a vector norm of a specific power, e.g. if params[0]==2 then it is the euclidian norm
 	float v=pow(x[0],params[0]) + pow(x[1],params[0]) + pow(x[2],params[0]);
@@ -56,7 +56,7 @@ float F3(const float *x, const float *params)
 	params. Don't worry too much about the details of the
 	matrix, it isn't relative to making it go fast.
 */
-float F4(const float *x, const float *params)
+__device__ float F4(const float *x, const float *params)
 {
 	// We need to calculate scale*exp(-x'*\Sigma^{-1}*x/2),
 	// where \Sigma^{-1} is a 3x3 matrix.
@@ -76,7 +76,7 @@ float F4(const float *x, const float *params)
 	k=3 (tri-variate)
 	params= [] (no parameters)
 */
-float F5(const float *x, const float *params)
+__device__ float F5(const float *x, const float *params)
 {
 	return powf(powf(sinf( pow(x[0], x[1]) ), 2), x[2]);
 }
@@ -86,7 +86,7 @@ float F5(const float *x, const float *params)
 	k=3 (tri-variate)
 	params= [radius,width]
 */
-float F6(const float *x, const float *params)
+__device__ float F6(const float *x, const float *params)
 {
 	float a=powf(powf(x[0],2)+powf(x[1],2)+powf(x[2],2),0.5); // distance from origin
 	float d=(a-params[0]);	// How far from surface of sphere
--- src/integ.cu
+++ src/integ.cu
@@ -7,23 +7,47 @@
 //	return sin(x); 
 //}
 
-__global__ void func_kernel(float * dy, float a, float base, int n)
+// define a function pointer type
+typedef float (*func)(const float *,const float *);
+__device__ func dF0 = F0;
+__device__ func dF1 = F1;
+__device__ func dF2 = F2;
+__device__ func dF3 = F3;
+__device__ func dF4 = F4;
+__device__ func dF5 = F5;
+__device__ func dF6 = F6;
+
+__global__ void func_kernel(func* F, float * dy, float a, float base, int n, int func_type)
 {
 	int id = blockIdx.x * blockDim.x + threadIdx.x;
 	// ensure we are within bounds
 	float x[1] = {a + base * ((float)0.5 + (float)id)};
 	if (id<n)
-		dy[id] = base * F0(x, NULL);
+		dy[id] = base * F[func_type](x, NULL);
 	__syncthreads();
 }
 
 
 int main( int argc, char* argv[])
 {
-	// define a function pointer type
-	//typedef float (*func)(int);
-	//__device__ func dF1 = F1;
-	
+	// allocatiing function pointers
+	int func_count = 7;
+	func * d_f;
+	func * h_f;	
+	h_f = (func*)malloc(func_count*sizeof(func));
+	cudaMalloc((void**)&d_f, func_count*sizeof(func));
+	cudaMemcpyFromSymbol( &h_f[0], dF0, sizeof(func));	
+	cudaMemcpyFromSymbol( &h_f[1], dF1, sizeof(func));	
+	cudaMemcpyFromSymbol( &h_f[2], dF2, sizeof(func));	
+	cudaMemcpyFromSymbol( &h_f[3], dF3, sizeof(func));	
+	cudaMemcpyFromSymbol( &h_f[4], dF4, sizeof(func));	
+	cudaMemcpyFromSymbol( &h_f[5], dF5, sizeof(func));	
+	cudaMemcpyFromSymbol( &h_f[6], dF6, sizeof(func));	
+	//	   dst				
+	//	    |	src
+	//	    |	 |	size
+	cudaMemcpy(d_f, h_f, func_count*sizeof(func), cudaMemcpyHostToDevice);
+
 	// vector size
 	int n = 4096; 
 	// device input/output vectors
@@ -52,7 +76,7 @@ int main( int argc, char* argv[])
 	int gridSize = (int) ceil((float)n/blockSize);
 	
 	//kernel execute
-	func_kernel<<<gridSize, blockSize>>>(dy, a, base, n);
+	func_kernel<<<gridSize, blockSize>>>(d_f, dy, a, base, n, 0);
 	
 	//copy array back
 	cudaMemcpy(y, dy, bytes, cudaMemcpyDeviceToHost);
