7316078cf8019dbce46356eefe717ee354a24837
Ubuntu <ubuntu@ip-10-33-5-97.eu-west-1.compute.internal>
2013-03-21 05:20:30 +0000

    3d works, also changed everything to double precision

--- src/functions.h
+++ src/functions.h
@@ -8,7 +8,7 @@
 	k=1 (uni-variate)
 	params = []  (no parameters)
 */
-__device__ float F0(const float *x, const float *params)
+__device__ double F0(const double *x, const double *params)
 { return exp(x[0]); }
 
 /* Name = sin(o1+x*y)*exp(o2+x)
@@ -16,7 +16,7 @@ __device__ float F0(const float *x, const float *params)
 	k=2 (bi-variate)
 	params = [o1,o2]  (2 parameters)
 */
-__device__ float F1(const float *x, const float *params)
+__device__ double F1(const double *x, const double *params)
 {
 	return sin(params[0] + x[0]*x[1]) * exp(params[1]+x[0]);
 }
@@ -26,7 +26,7 @@ __device__ float F1(const float *x, const float *params)
 	k=3 (tri-variate)
 	params = []  (no parameters)
 */
-__device__ float F2(const float *x, const float *params)
+__device__ double F2(const double *x, const double *params)
 {
 	return round(exp(-x[0]))-round(exp(x[1]))*sin(x[2]);
 }
@@ -36,10 +36,10 @@ __device__ float F2(const float *x, const float *params)
 	k=3 (tri-variate)
 	params = [norm_lev]  (1 parameter)
 */
-__device__ float F3(const float *x, const float *params)
+__device__ double F3(const double *x, const double *params)
 {
 	// Perform a vector norm of a specific power, e.g. if params[0]==2 then it is the euclidian norm
-	float v=pow(x[0],params[0]) + pow(x[1],params[0]) + pow(x[2],params[0]);
+	double v=pow(x[0],params[0]) + pow(x[1],params[0]) + pow(x[2],params[0]);
 	v=pow(v,1/params[0]);
 	// Then a sinc
 	return sin(v)/v;
@@ -56,15 +56,15 @@ __device__ float F3(const float *x, const float *params)
 	params. Don't worry too much about the details of the
 	matrix, it isn't relative to making it go fast.
 */
-__device__ float F4(const float *x, const float *params)
+__device__ double F4(const double *x, const double *params)
 {
 	// We need to calculate scale*exp(-x'*\Sigma^{-1}*x/2),
 	// where \Sigma^{-1} is a 3x3 matrix.
 	int i;
-	float acc=0;
+	double acc=0;
 	for(i=0;i<3;i++){
 		// We first form a row of the \Sigma^-1 * x
-		float row= params[i*3+0]*x[0] + params[i*3+1]*x[1] + params[i*3+2]*x[2];
+		double row= params[i*3+0]*x[0] + params[i*3+1]*x[1] + params[i*3+2]*x[2];
 		// Then perform the left mutiply with x'
 		acc += row*x[i];
 	}
@@ -76,7 +76,7 @@ __device__ float F4(const float *x, const float *params)
 	k=3 (tri-variate)
 	params= [] (no parameters)
 */
-__device__ float F5(const float *x, const float *params)
+__device__ double F5(const double *x, const double *params)
 {
 	return powf(powf(sinf( pow(x[0], x[1]) ), 2), x[2]);
 }
@@ -86,10 +86,10 @@ __device__ float F5(const float *x, const float *params)
 	k=3 (tri-variate)
 	params= [radius,width]
 */
-__device__ float F6(const float *x, const float *params)
+__device__ double F6(const double *x, const double *params)
 {
-	float a=powf(powf(x[0],2)+powf(x[1],2)+powf(x[2],2),0.5); // distance from origin
-	float d=(a-params[0]);	// How far from surface of sphere
+	double a=powf(powf(x[0],2)+powf(x[1],2)+powf(x[2],2),0.5); // distance from origin
+	double d=(a-params[0]);	// How far from surface of sphere
 	if(d<0)
 		d=-d;
 	if(d<params[1]){
--- src/integ.cu
+++ src/integ.cu
@@ -1,21 +1,23 @@
 #include <stdio.h>
+#include <stdint.h>
 #include <stdlib.h>
 #include "functions.h"
 
 
 
-__global__ void func_kernel(float * dy, float* a, float* base, float * params, int n)
+__global__ void func_kernel(double * dy, double* a, double* base, double * params, int n)
 {
-	int idx = blockIdx.x * blockDim.x + threadIdx.x;
-	int idy = blockIdx.y * blockDim.y + threadIdx.y;
-	int offset = idx + idy * blockDim.x * gridDim.x; 
-
+	uint32_t idx = blockIdx.x * blockDim.x + threadIdx.x;
+	uint32_t idy = blockIdx.y * blockDim.y + threadIdx.y;
+	uint32_t idz = blockIdx.z * blockDim.z + threadIdx.z;
+	uint32_t offset = idx + (idy + (blockDim.x * idz * gridDim.x))  * blockDim.x * gridDim.x; 
+//	printf("%d, %d, %d, %d\n", idx, idy, idz, offset);
 	// ensure we are within bounds
-	float x[2] = {a[0] + base[0] * (0.5f + (float)idx), a[1] + base[1] * (0.5f + (float)idy)};
+	double x[3] = {a[0] + base[0] * (0.5f + (double)idx), a[1] + base[1] * (0.5f + (double)idy), a[2] + base[2] * (0.5f + (double)idz),};
 	__syncthreads();
-	if (idx< n && idy<n) {
-		dy[offset] = F1(x, params) ;
-		for (int j=0; j<2; j++) {
+	if (idx< n && idy<n && idz<n) {
+		dy[offset] = F3(x, params) ;
+		for (int j=0; j<3; j++) {
 			dy[offset] *= base[j];
 		}
 	}
@@ -27,55 +29,53 @@ void cudasafe( cudaError_t error, char* message)
 
 double Integrate(
     int functionCode, // Identifies the function (and dimensionality k)
-    const float *a, // An array of k lower bounds
-    const float *b, // An array of k upper bounds
-    float n, // A target accuracy
-    const float *params, // Parameters to function
-    float *errorEstimate // Estimated error in integral
+    const double *a, // An array of k lower bounds
+    const double *b, // An array of k upper bounds
+    double n, // A target accuracy
+    const double *params, // Parameters to function
+    double *errorEstimate // Estimated error in integral
 ) 
 {
-	//int n = 32; 
-	// device input/output vectors
-	
 	// size, in bytes, of each vector
-	size_t bytes = (n*n)*sizeof(float);
+	size_t bytes = (n*n*n)*sizeof(double);
 		
-	float *y = (float*)malloc(bytes);
-	
- 	
+	double *y = (double*)malloc(bytes);
 
-	float base[2] = {(b[0] - a[0]) / (float)n, (b[1] - a[1]) / (float)n};
+//	double base[2] = {(b[0] - a[0]) / (double)n, (b[1] - a[1]) / (double)n};
+	
+	double base[3] = {(b[0] - a[0]) / (double)n, (b[1] - a[1]) / (double)n, (b[2] - a[2]) / (double)n};
 
 	// allocate memory for each vector on GPU
-	float * dy;
-	float * dbase;
-	float * da;
-	float * dparams;
+	double * dy;
+	double * dbase;
+	double * da;
+	double * dparams;
+	
 	cudaMalloc(&dy, bytes);
 	cudaMalloc(&dbase, sizeof(base));
 	cudaMalloc(&da, sizeof(a));
 	cudaMalloc(&dparams, sizeof(params));
-	// allocate memory for params	
+
 	// number of threads in each thread block
-	int blockSize = 32;
-	dim3 dimBlock(blockSize, blockSize);
+	int blockSize = 8;
+	dim3 dimBlock(blockSize, blockSize, blockSize);
 
 	// number of thread blocks in grid
-	int gridSize = (int) ceil((float)n/blockSize);
-	dim3 dimGrid(gridSize, gridSize);
+	int gridSize = (int) ceil((double)n/blockSize);
+	dim3 dimGrid(gridSize, gridSize, gridSize);
 	
 	cudaMemcpy(dbase, base, sizeof(base), cudaMemcpyHostToDevice);
 	cudaMemcpy(da, a, sizeof(a), cudaMemcpyHostToDevice);
 	cudaMemcpy(dparams, params, sizeof(params), cudaMemcpyHostToDevice);
+
 	//kernel execute
 	func_kernel<<<dimGrid, dimBlock>>>(dy, da, dbase, dparams, n);
 	
 	//copy array back
 	cudaMemcpy(y, dy, bytes, cudaMemcpyDeviceToHost);
 	
-	float sum = 0;
-
-	for(int i=0; i<n*n; i++) {
+	double sum = 0;
+	for(uint32_t i=0; i<n*n*n; i++) {
 		sum += y[i];
 	}
 	printf("final result: %f\n", sum);
@@ -89,12 +89,35 @@ double Integrate(
 
 	return sum;
 }
+void test1(void) {
+	double a[2]={0,0};
+	double b[2]={1,1};
+	double params[2]={0.5,0.5};
+	double error;
+	int n = 32; 
+	Integrate(1, a, b, n, params, &error); 	
+}
+
+void test2(void) {
+	double exact=9.48557252267795;	// Correct to about 6 digits
+	double a[3]={-1,-1,-1};
+	double b[3]={1,1,1};
+	int n = 8;	
+	double error;
+	Integrate(2, a, b, n, NULL, &error); 	
+}
+
+void test3(void) {
+	double exact=-7.18387139942142f;	// Correct to about 6 digits
+	double a[3]={0,0,0};
+	double b[3]={5,5,5};
+	double params[1]={2};
+	int n = 512;	
+	double error;
+	Integrate(3, a, b, n, params, &error); 	
+}
 
 int main( int argc, char* argv[]) {
-	float a[2]={0,0};
-	float b[2]={1,1};
-	float params[2]={0.5,0.5};
-	float error;
-	int n = atoi(argv[1]);
-	Integrate(2, a, b, n, params, &error); 	
+	test3();
 }
+
