d7b24cda0d5f09b78ceb3dbafd393e26e0a92c2b
Ubuntu <ubuntu@ip-10-33-5-97.eu-west-1.compute.internal>
2013-03-21 04:10:19 +0000

    added function integrate

--- src/integ.cu
+++ src/integ.cu
@@ -4,49 +4,45 @@
 
 
 
-__global__ void func_kernel(float * dy, float* a, float* base, int n)
+__global__ void func_kernel(float * dy, float* a, float* base, float * params, int n)
 {
 	int idx = blockIdx.x * blockDim.x + threadIdx.x;
 	int idy = blockIdx.y * blockDim.y + threadIdx.y;
-	int offset = idx + idy * blockDim.x ; 
-	__syncthreads();
-	printf("%d, %d, %d\n", idx, idy, offset);
-//	printf("%d\n",blockIdx.x);
-	float params[2]={0.5,0.5};
+	int offset = idx + idy * blockDim.x * gridDim.x; 
 
 	// ensure we are within bounds
 	float x[2] = {a[0] + base[0] * (0.5f + (float)idx), a[1] + base[1] * (0.5f + (float)idy)};
 	__syncthreads();
 	if (idx< n && idy<n) {
 		dy[offset] = F1(x, params) ;
-		for (int j=0; j<2; j++)
+		for (int j=0; j<2; j++) {
 			dy[offset] *= base[j];
+		}
 	}
-	__syncthreads();
 }
 void cudasafe( cudaError_t error, char* message)
 {
    if(error!=cudaSuccess) { fprintf(stderr,"ERROR: %s : %i\n",message,error); exit(-1); }
 }
 
-int main( int argc, char* argv[])
+double Integrate(
+    int functionCode, // Identifies the function (and dimensionality k)
+    const float *a, // An array of k lower bounds
+    const float *b, // An array of k upper bounds
+    float n, // A target accuracy
+    const float *params, // Parameters to function
+    float *errorEstimate // Estimated error in integral
+) 
 {
 	//int n = 32; 
 	// device input/output vectors
-	int n = atoi(argv[1]);
 	
 	// size, in bytes, of each vector
 	size_t bytes = (n*n)*sizeof(float);
 		
 	float *y = (float*)malloc(bytes);
 	
-	// float a, b;
-//	int functionCode = atoi(argv[1]);
-	// sscanf(argv[2], "%f", &a); 
-	// sscanf(argv[3], "%f", &b);
  	
-	float a[2]={0,0};
-	float b[2]={1,1};
 
 	float base[2] = {(b[0] - a[0]) / (float)n, (b[1] - a[1]) / (float)n};
 
@@ -54,9 +50,11 @@ int main( int argc, char* argv[])
 	float * dy;
 	float * dbase;
 	float * da;
+	float * dparams;
 	cudaMalloc(&dy, bytes);
 	cudaMalloc(&dbase, sizeof(base));
 	cudaMalloc(&da, sizeof(a));
+	cudaMalloc(&dparams, sizeof(params));
 	// allocate memory for params	
 	// number of threads in each thread block
 	int blockSize = 32;
@@ -68,8 +66,9 @@ int main( int argc, char* argv[])
 	
 	cudaMemcpy(dbase, base, sizeof(base), cudaMemcpyHostToDevice);
 	cudaMemcpy(da, a, sizeof(a), cudaMemcpyHostToDevice);
+	cudaMemcpy(dparams, params, sizeof(params), cudaMemcpyHostToDevice);
 	//kernel execute
-	func_kernel<<<dimGrid, dimBlock>>>(dy, da, dbase, n);
+	func_kernel<<<dimGrid, dimBlock>>>(dy, da, dbase, dparams, n);
 	
 	//copy array back
 	cudaMemcpy(y, dy, bytes, cudaMemcpyDeviceToHost);
@@ -84,9 +83,18 @@ int main( int argc, char* argv[])
 	cudaFree(dy);
 	cudaFree(da);
 	cudaFree(dbase);
+	cudaFree(dparams);
 
 	free(y);
 
-	return 0;
+	return sum;
 }
 
+int main( int argc, char* argv[]) {
+	float a[2]={0,0};
+	float b[2]={1,1};
+	float params[2]={0.5,0.5};
+	float error;
+	int n = atoi(argv[1]);
+	Integrate(2, a, b, n, params, &error); 	
+}
