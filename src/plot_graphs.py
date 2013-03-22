c8ab1296749f417dec654782a6d210169187b662
Mo Mirza <mo.mirza@iwoca.co.uk>
2013-03-22 15:50:24 +0000

    changed plotting script

--- src/plot_graphs.py
+++ src/plot_graphs.py
@@ -7,17 +7,8 @@ import sys
 
 
 
-squares = [2**n for n in xrange(11)]
 
-RUNS = 20
-
-def call_executable(file):
-    proc_list = []
-    get_times = lambda e : map(float, e[0].split())
-    for _ in xrange(RUNS):
-        proc_list.append(get_times(Popen(['./%s.out', file, str(square)],stdout=PIPE).stdout.readlines()))
-    sums = reduce(lambda x,y: [x[0]+y[0], x[1]+y[1], x[2]+y[2], x[3]+y[3]], proc_list)
-    return  map(lambda e: e/RUNS, sums)
+RUNS = 10
 
 def dump_csvs(times):
     """ takes in data as list and generates csv """
@@ -26,45 +17,56 @@ def dump_csvs(times):
         writer.writerow([squares[n]] + time)
 
 def call_exec(file):
-	proc_list = []
-	get_times = lambda x: x.split()
-	for _ in xrange(RUNS):
-		proc = map(get_times, (Popen(['./test'] , stdout=PIPE).stdout.readlines()))
-	import pdb; pdb.set_trace()
-	pass
+    proc_list = []
+    get_times = lambda x: map(float, x.split())
+    for _ in xrange(RUNS):
+        proc_list.append(map(get_times, (Popen([('./%s' %file)] , stdout=PIPE).stdout.readlines())))
+    averaged = [[map(np.mean, zip(*[x[y] for x in proc_list]))] for y in range(len(proc_list[0]))]
+    averaged = [x[0] for x in averaged]
+    return [[[int(x[1]), x[2]] for x in averaged if int(x[0])==y] for y in range(3)]
+    # return [[[int(x[1]), np.log10(0.000001) if not x[2] else np.log10(x[2])] for x in averaged if int(x[0])==y] for y in range(3)]
 
 if __name__ == "__main__":
     DATA_EXISTS = False if sys.argv[-1] == 'rerun' else True
 
     if DATA_EXISTS:
-        reader = csv.reader(open('mat.csv', 'r'))
+        reader = csv.reader(open('data.csv', 'r'))
         times = []
         for row in reader:
             times.append(map(float, row))
         squares, ser, tbb, opt, eli = zip(*times)  
     else:
-        times = map(call_executable, squares)
-        ser, tbb, opt, eli = zip(*times)
-        dump_csvs(times)
+        F0, F1, F2 = call_exec("dumb")
+        # dump_csvs(times)
+    nF0, F0 = zip(*F0)
+    nF1, F1 = zip(*F1)
+    nF2, F2 = zip(*F2)
+    # nF0, F0 = zip(*F0)
+    # nF0, F0 = zip(*F0)
+    # nF0, F0 = zip(*F0)
+
     
-    
-    tck_ser = interpolate.splrep(squares,ser,s=0)
-    tck_tbb = interpolate.splrep(squares,tbb,s=0)
-    tck_opt = interpolate.splrep(squares,opt,s=0)
-    tck_eli = interpolate.splrep(squares,eli,s=0)
-    squaresnew = np.arange(1, max(squares) + 1, 10)
-    ser_i = interpolate.splev(squaresnew,tck_ser ,der=0)
-    tbb_i = interpolate.splev(squaresnew,tck_tbb ,der=0)
-    opt_i = interpolate.splev(squaresnew,tck_opt ,der=0)
-    eli_i = interpolate.splev(squaresnew,tck_eli ,der=0)
+    tck_F0 = interpolate.splrep(nF0,F0,s=0)
+    tck_F1 = interpolate.splrep(nF1,F1,s=0)
+    tck_F2 = interpolate.splrep(nF2,F2,s=0)
+    # tck_F3 = interpolate.splrep(nF3,F3,s=0)
+    nF0new = np.arange(1, max(nF0) + 1, 10)
+    nF1new = np.arange(1, max(nF1) + 1, 10)
+    nF2new = np.arange(1, max(nF2) + 1, 10)
+    # nF3new = np.arange(1, max(nF3) + 1, 10)
+    F0_i = interpolate.splev(nF0new,tck_F0 ,der=0)
+    F1_i = interpolate.splev(nF1new,tck_F1 ,der=0)
+    F2_i = interpolate.splev(nF2new,tck_F2 ,der=0)
+    # F3_i = interpolate.splev(nF3,tck_F3 ,der=0)
 
 
-    pyplot.plot(squaresnew, ser_i,'-r', squaresnew, tbb_i,'-b', squaresnew, opt_i,'-k', squaresnew, eli_i,'-g', lw= 1.5)
-    pyplot.plot(squares , ser, 'ro', squares, tbb, 'bo', squares, opt, 'ko', squares, eli, 'go')
+    pyplot.plot(nF0new, F0_i,'-r', nF1new, F1_i,'-b', nF2new, F2_i,'-k', lw= 1.5)
+    pyplot.plot(nF0 , F0, 'ro', nF1, F1, 'bo', nF2, F2, 'ko')
     pyplot.grid(which='major', axis='both', linestyle='-',color='#C0C0C0', alpha=0.5)
     pyplot.grid(which='minor', axis='y', linestyle='--',color='#C0C0C0', alpha=0.5)
     pyplot.xlabel("n")
-    pyplot.legend(('Serial','TBB', 'OPT', 'Elision'), loc=2)
+    # pyplot.ylim(ymin=0)
+    pyplot.legend(('F0','F1', 'F2'), loc=2)
     pyplot.ylabel("Time (s)")
-    pyplot.title("Execution Times for matrix-matrix multiplication (%d simulation runs) " % RUNS)
-    pyplot.savefig('mat.pdf')
+    pyplot.title("Execution Times - Serial (%d simulation runs) " % RUNS)
+    pyplot.savefig('serial.pdf')
